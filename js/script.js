388927ed2cb7f2d42f0ec44161bb5abe4f2cd611
Sampo01 <simon.metivier79@gmail.com>
2023-12-08 10:40:35 +0100

    Simon 10h40

---
+++ js/script.js
@@ -0,0 +1,119 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const popup = document.getElementById('Buanespop');
+    const button = document.getElementById('Buanesbutton');
+    const closePopup = document.getElementById('Buanesclose');
+
+    button.addEventListener('click', function () {
+        popup.style.display = 'block';
+    });
+
+    closePopup.addEventListener('click', function () {
+        popup.style.display = 'none';
+    });
+
+    window.addEventListener('click', function (event) {
+        if (event.target === popup) {
+            popup.style.display = 'none';
+        }
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const popup = document.getElementById('Rabier-hautpop');
+    const button = document.getElementById('Rabier-hautbutton');
+    const closePopup = document.getElementById('Rabier-hautclose');
+
+    button.addEventListener('click', function () {
+        popup.style.display = 'block';
+    });
+
+    closePopup.addEventListener('click', function () {
+        popup.style.display = 'none';
+    });
+
+    window.addEventListener('click', function (event) {
+        if (event.target === popup) {
+            popup.style.display = 'none';
+        }
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const popup = document.getElementById('Bremaudpop');
+    const button = document.getElementById('Bremaudbutton');
+    const closePopup = document.getElementById('Bremaudclose');
+
+    button.addEventListener('click', function () {
+        popup.style.display = 'block';
+    });
+
+    closePopup.addEventListener('click', function () {
+        popup.style.display = 'none';
+    });
+
+    window.addEventListener('click', function (event) {
+        if (event.target === popup) {
+            popup.style.display = 'none';
+        }
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const popup = document.getElementById('Rabier-baspop');
+    const button = document.getElementById('Rabier-basbutton');
+    const closePopup = document.getElementById('Rabier-basclose');
+
+    button.addEventListener('click', function () {
+        popup.style.display = 'block';
+    });
+
+    closePopup.addEventListener('click', function () {
+        popup.style.display = 'none';
+    });
+
+    window.addEventListener('click', function (event) {
+        if (event.target === popup) {
+            popup.style.display = 'none';
+        }
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const popup = document.getElementById('Papinpop');
+    const button = document.getElementById('Papinbutton');
+    const closePopup = document.getElementById('Papinclose');
+
+    button.addEventListener('click', function () {
+        popup.style.display = 'block';
+    });
+
+    closePopup.addEventListener('click', function () {
+        popup.style.display = 'none';
+    });
+
+    window.addEventListener('click', function (event) {
+        if (event.target === popup) {
+            popup.style.display = 'none';
+        }
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const popup = document.getElementById('fildutemppop');
+    const button = document.getElementById('fildutempbutton');
+    const closePopup = document.getElementById('fildutempclose');
+
+    button.addEventListener('click', function () {
+        popup.style.display = 'block';
+    });
+
+    closePopup.addEventListener('click', function () {
+        popup.style.display = 'none';
+    });
+
+    window.addEventListener('click', function (event) {
+        if (event.target === popup) {
+            popup.style.display = 'none';
+        }
+    });
+});
