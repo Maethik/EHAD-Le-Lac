81bf714faf0614e2bf29201123a7f6574528bdd5
Maethik <matheo.guilbert49@gmail.com>
2023-12-09 17:59:42 +0100

    Mathéo 9dec

--- js/navBar.js
+++ js/navBar.js
@@ -1,7 +1,32 @@
-const presentationLink = document.querySelector("presentation-link");
-const sousMenuPresentation = document.querySelector("sous-menu-presentation");
+document.addEventListener('DOMContentLoaded', function() {
+    const triggerButton = document.getElementById('presentation-link');
+    const targetElement = document.getElementById('sous-menu-presentation');
 
-presentationLink.addEventListener("click", null, () => {
-    console.log('OUIII');
-    sousMenuPresentation.classList.toggle("active");
+    triggerButton.addEventListener('mouseenter', function() {
+        changeVisibility(targetElement);
+    });
 });
+
+function changeVisibility(element) {
+    if (element.style.visibility === 'hidden' || getComputedStyle(element).visibility === 'hidden') {
+        element.style.visibility = 'visible';
+    } else {
+        element.style.visibility = 'hidden';
+    }
+}
+
+const navbar = document.getElementById('header');
+const background = document.getElementById('background');
+const logo = document.getElementById('logo');
+
+window.onscroll = function() {
+    if (document.body.scrollTop > 1 || document.documentElement.scrollTop > 1) {
+        navbar.classList.add('small');
+        background.classList.add('small');
+        logo.classList.add('small');
+    } else {
+        navbar.classList.remove('small');
+        background.classList.remove('small');
+        logo.classList.remove('small');
+    }
+};
